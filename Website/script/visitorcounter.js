c3118a35879d6a474bb98d40765158c27491b087
Johnathan Vega <112981731+Johnathan-Vega@users.noreply.github.com>
2022-11-25 02:24:38 -0600

    Delete Website directory
    Deleting directory with capital W in "Website" directory to correspond with Github action template change

--- Website/script/visitorcounter.js
+++
@@ -1,7 +0,0 @@
-function countFunction() {
-  displayCount = document.getElementById('countscript');
-  apiUrl = 'https://4xhcs260a5.execute-api.us-east-1.amazonaws.com/serverless_lambda_stage/visitor/count'
-  fetch(apiUrl).then((response) =>
-      response.json().then((data) => displayCount.innerHTML = data.CountAmount));
-}
-window.onload = countFunction();
